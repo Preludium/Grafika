faed5ba8c3a52bb3654604d4e73532e32ee0e6e9
bartek-s13 <43931596+bartek-s13@users.noreply.github.com>
2019-06-23 10:18:18 +0200

    '''

--- src/seven.cpp
+++ src/seven.cpp
@@ -46,6 +46,37 @@ void seven::RotL(cube (&cubemap)[9][12][9])
         parts[0].z = parts[1].z - 1;
 
 
+        this->state = 2;
+        break;
+
+    case 2:
+
+        parts[3].x = parts[2].x;
+        parts[3].z = parts[2].z + 1;
+        parts[4].x = parts[3].x - 1;
+        parts[4].z = parts[3].z;
+
+        parts[1].x = parts[2].x;
+        parts[1].z = parts[2].z - 1;
+        parts[0].x = parts[1].x + 1;
+        parts[0].z = parts[1].z;
+
+        this->state = 3;
+        break;
+
+    case 3:
+
+        parts[3].x = parts[2].x + 1;
+        parts[3].z = parts[2].z;
+        parts[4].x = parts[3].x;
+        parts[4].z = parts[3].z + 1;
+
+        parts[1].x = parts[2].x - 1;
+        parts[1].z = parts[2].z;
+        parts[0].x = parts[1].x;
+        parts[0].z = parts[1].z - 1;
+
+
         this->state = 0;
         break;
     }
@@ -84,7 +115,45 @@ void seven::RotR(cube (&cubemap)[9][12][9])
         parts[0].z = parts[1].z - 1;
 
 
+        this->state = 2;
+        break;
+
+    case 2:
+
+        parts[3].x = parts[2].x;
+        parts[3].z = parts[2].z + 1;
+        parts[4].x = parts[3].x - 1;
+        parts[4].z = parts[3].z;
+
+        parts[1].x = parts[2].x;
+        parts[1].z = parts[2].z - 1;
+        parts[0].x = parts[1].x + 1;
+        parts[0].z = parts[1].z;
+
+        this->state = 3;
+        break;
+
+    case 3:
+
+        parts[3].x = parts[2].x + 1;
+        parts[3].z = parts[2].z;
+        parts[4].x = parts[3].x;
+        parts[4].z = parts[3].z + 1;
+
+        parts[1].x = parts[2].x - 1;
+        parts[1].z = parts[2].z;
+        parts[0].x = parts[1].x;
+        parts[0].z = parts[1].z - 1;
+
+
         this->state = 0;
         break;
+
+
+
+
+
+
+
     }
 }
