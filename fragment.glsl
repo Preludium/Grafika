52734ee9948f1bc06963194cadce14c45ff27445
bartek-s13 <43931596+bartek-s13@users.noreply.github.com>
2019-06-26 09:40:50 +0200

    koniec?

--- fragment.glsl
+++ fragment.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-in vec4 l;
+in vec4 l1;
 in vec4 l2;
 in vec4 n;
 in vec4 v;
@@ -11,25 +11,25 @@ uniform sampler2D textureMap0;
 out vec4 pixelColor;
 
 void main(void) {
-    vec4 ld=vec4(1,1,1,1); 
-    vec4 ks=vec4(1,1,1,1); 
-    vec4 ls=vec4(1,1,1,1); 
+    vec4 ld=vec4(1,1,1,1);
+    vec4 ks=vec4(1,1,1,1);
+    vec4 ls=vec4(1,1,1,1);
 
     vec4 kd=texture(textureMap0,iTexCoord0);
 
-    vec4 ml=normalize(l);
+    vec4 ml1=normalize(l1);
     vec4 ml2=normalize(l2);
     vec4 mn=normalize(n);
     vec4 mv=normalize(v);
-    vec4 mr=reflect(-ml,mn); 
-    vec4 mr2=reflect(-ml2,mn); 
+    vec4 mr1=reflect(-ml1,mn);
+    vec4 mr2=reflect(-ml2,mn);
 
-    float nl=clamp(dot(mn,ml),0,1); 
-    float nl2=clamp(dot(mn,ml2),0,1); 
-    float rv=pow(clamp(dot(mr,mv),0,1),25); 
-    float rv2=pow(clamp(dot(mr2,mv),0,1),25); 
+    float nl1=clamp(dot(mn,ml1),0,1);
+    float nl2=clamp(dot(mn,ml2),0,1);
+    float rv1=pow(clamp(dot(mr1,mv),0,1),25);
+    float rv2=pow(clamp(dot(mr2,mv),0,1),25);
 
-	vec4 color=vec4(kd.rgb*ld.rgb*nl+ks.rgb*ls.rgb*rv,kd.a);
+	vec4 color1=vec4(kd.rgb*ld.rgb*nl1+ks.rgb*ls.rgb*rv1,kd.a);
     vec4 color2=vec4(kd.rgb*ld.rgb*nl2+ks.rgb*ls.rgb*rv2,kd.a);
-	pixelColor=clamp(color*.7+color2*0.7,0,1);
+	pixelColor = (color1+color2)/2;
 }
--- vertex.glsl
+++ vertex.glsl
@@ -3,14 +3,14 @@
 uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
-uniform vec4 lp;
+uniform vec4 lp1;
 uniform vec4 lp2;
 
 in vec4 vertex;
 in vec4 normal;
 in vec2 texCoord0;
 
-out vec4 l;
+out vec4 l1;
 out vec4 l2;
 out vec4 n;
 out vec4 v;
@@ -18,9 +18,9 @@ out vec2 iTexCoord0;
 
 void main(void) {
 
-    l=normalize(V*(lp-M*vertex));
-    l2=normalize(V*(lp2-M*vertex)); 
-    n=normalize(V*M*normal); 
+    l1=normalize(V*(lp1-M*vertex));
+    l2=normalize(V*(lp2-M*vertex));
+    n=normalize(V*M*normal);
     v=normalize(vec4(0,0,0,1)-V*M*vertex);
 
     iTexCoord0=texCoord0;
